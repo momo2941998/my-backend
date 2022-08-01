26fc52945c07de320feb6e85b2f79d734d4be2b0
Nguyen Quang Thanh <thanh.nguyenquang@mobifone.vn>
2022-08-01 15:47:45 +0700

    logging error object instead of message only

--- src/middleware/errorMiddleware.ts
+++ src/middleware/errorMiddleware.ts
@@ -3,7 +3,7 @@ import { HttpException } from '../exceptions';
 import { logger } from '../logger';
 function errorMiddleware(error: Error, request: Request, response: Response, next: NextFunction) {
   if (error instanceof HttpException) {
-    logger.error(error.message)
+    logger.error(error)
     return response.status(error.status).json({
         code: error.code,
         error: error.message,
